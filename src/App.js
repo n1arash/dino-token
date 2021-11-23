de455c2287f3ebe1f7df7bc505f663204319e513
Arash Khazaei <n1arash@hamravesh.com>
2021-11-24 00:02:20 +0330

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import 'regenerator-runtime/runtime'
+import React from 'react'
+import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+import getConfig from './config'
+import { initContract } from './utils'
+const { contractName } = getConfig(process.env.NODE_ENV || 'development')
+
+export default function App() {
+  const [{accountId, contract, walletConnection}, setInitContract] = React.useState({})
+  const [isSignedIn, setIsSignedIn] = React.useState(false)
+  React.useEffect(
+    () => {
+      async function init() {
+
+        const { accountId, contract, walletConnection } = await initContract()
+        setInitContract({accountId, contract, walletConnection})
+        setIsSignedIn(walletConnection.isSignedIn())
+      }
+      init()
+    },
+    [setInitContract, setIsSignedIn]
+  )
+
+  function logout() {
+  walletConnection.signOut()
+  // reload page
+  window.location.replace(window.location.origin + window.location.pathname)
+  }
+
+  function login() {
+    walletConnection.requestSignIn(contractName)
+  }
+
+
+  // if not signed in, return early with sign-in prompt
+  if (!isSignedIn) {
+    return (
+      <main>
+        <h1>Welcome to Blue Dino Token</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          To make use of the NEAR blockchain, you need to sign in. The button
+          below will sign you in using NEAR Wallet.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <p style={{ textAlign: 'center', marginTop: '2.5em' }}>
+          <button onClick={login}>Sign in</button>
+        </p>
+      </main>
+    )
+  }
+  return (
+    <main>
+      <button onClick={logout}>Logout</button>
+    </main>
+  )
 
-export default App;
+}
